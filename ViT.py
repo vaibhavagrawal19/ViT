a5f49f25e6afa8cab5dc07f7b3c51ff3f7228de5
Vaibhav Agrawal <agrvaibhav19@gmail.com>
2023-10-01 23:10:29 +0530

    ViT runs, but the accuracy does not converge :(

--- ViT.py
+++ ViT.py
@@ -8,6 +8,13 @@ import math
 
 from tqdm import tqdm, trange
 
+
+from torchvision.transforms import ToTensor
+from torchvision.datasets.mnist import MNIST
+
+LR = 0.005
+NUM_EPOCHS = 40
+
 class PatchEmbedding(nn.Module):
     def __init__(self, image_dim: tuple, patch_dim: tuple, in_channels: int, embed_dim: int):
         super().__init__()
@@ -20,20 +27,22 @@ class PatchEmbedding(nn.Module):
     def forward(self, x):
         n = x.shape[0]
         x = self.projection(x)
-        x = x.view(n, self.embed_dim, -1)
+        x = x.reshape(x.shape[0], x.shape[1], x.shape[2] * x.shape[3])
         x = torch.transpose(x, -2, -1)
         return x
-        
+
 
 
 class ViT(nn.Module):
-    def __init__(self, image_dim: tuple, in_channels=3, patch_dim=(16, 16), hidden_dim=512, n_heads=8, out_dim=10):
+    def __init__(self, image_dim: tuple, device, in_channels=3, n_encoders=1, patch_dim=(16, 16), hidden_dim=512, n_heads=8, out_dim=10):
         super().__init__()
         self.image_dim = image_dim
         self.in_channels = in_channels
         self.patch_dim = patch_dim
         self.n_heads = n_heads
         self.out_dim = out_dim
+        self.device = device
+        self.n_encoders = n_encoders
         n_patches_x = image_dim[0] // patch_dim[0] if image_dim[0] % patch_dim[0] == 0 else image_dim[0] // patch_dim[0] + 1
         n_patches_y = image_dim[1] // patch_dim[1] if image_dim[1] % patch_dim[1] == 0 else image_dim[1] // patch_dim[1] + 1
         self.n_patches = n_patches_x * n_patches_y
@@ -41,7 +50,7 @@ class ViT(nn.Module):
         self.pos_embed = nn.Parameter(self.get_pos_embed(self.n_patches + 1))
         self.pos_embed.requires_grad = False
         self.class_token = nn.Parameter(torch.empty((self.hidden_dim, )))
-        self.encoders = nn.ModuleList([Encoder(self.hidden_dim, self.n_heads) for _ in range(self.n_heads)])
+        self.encoders = nn.ModuleList([Encoder(self.hidden_dim, self.n_heads) for _ in range(self.n_encoders)])
         self.encoders = nn.Sequential(*(self.encoders))
         self.mlp = nn.Linear(self.hidden_dim, self.out_dim)
 
@@ -54,11 +63,11 @@ class ViT(nn.Module):
     
     def forward(self, x):
         n = x.shape[0]
-        patchify = PatchEmbedding(self.image_dim, self.patch_dim, self.in_channels, self.hidden_dim)
+        patchify = PatchEmbedding(self.image_dim, self.patch_dim, self.in_channels, self.hidden_dim).to(self.device)
         patch_embeddings = patchify(x)
-        embeddings = torch.empty(n, patch_embeddings.shape[1] + 1, self.hidden_dim)
+        embeddings = torch.empty(n, patch_embeddings.shape[1] + 1, self.hidden_dim).to(device)
         for i in range(n):
-            embeddings[i] = torch.cat([patch_embeddings[i], nn.Parameter(torch.empty((self.hidden_dim, )))[None, :]])
+            embeddings[i] = torch.cat([patch_embeddings[i], self.class_token[None, :]])
             embeddings[i] = embeddings[i] + self.pos_embed
         features = self.encoders(patch_embeddings)[:, 0]
         return self.mlp(features)
@@ -85,18 +94,15 @@ class MultiHeadAttention(nn.Module):
                 q = self.Q[head](sequence)
                 k = self.K[head](sequence)
                 v = self.V[head](sequence)
-                # assert q.shape == (len(sequence), self.v_dim)
-                # assert k.shape == (len(sequence), self.v_dim)
-                # assert v.shape == (len(sequence), self.v_dim)
                 scores = self.softmax((q @ k.T) / math.sqrt(self.v_dim))
-                # assert scores.shape == (len(sequence), len(sequence))
                 z = scores @ v
                 seq_result.append(z)
             results.append(seq_result)
         results = [torch.cat([head_result for head_result in seq_result], dim=-1) for seq_result in results]
-        results = torch.cat([result[None, :] for result in results])
+        results = torch.cat([result[None, :] for result in results], dim=0)
         return results
     
+
 
 class Encoder(nn.Module):
     def __init__(self, hidden_dim=512, n_heads=8):
@@ -112,7 +118,6 @@ class Encoder(nn.Module):
         )
 
     def forward(self, x):
-        n = x.shape[0]
         x_ = x.clone()
         x = self.layer_norm(x)
         x = self.attention(x)
@@ -125,12 +130,43 @@ class Encoder(nn.Module):
         return x
     
 
+
 if __name__ == "__main__":
-    batch_size = 10
-    n_words = 20
-    embed_dim = 512
-    n_heads = 8
-    model = ViT((224, 224))
-    x = torch.rand(batch_size, 3, 224, 224)
-    out = model(x)
-    print(out.shape)
+    transform = ToTensor()
+    train_set = MNIST(root='./datasets', train=True, download=True, transform=transform)
+    test_set = MNIST(root='./datasets', train=False, download=True, transform=transform)
+    train_loader = DataLoader(train_set, shuffle=True, batch_size=10)
+    test_loader = DataLoader(test_set, shuffle=False, batch_size=10)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"using {device}")
+    # device = torch.device("cpu")
+    model = ViT((28, 28), device, in_channels=1, n_encoders=1).to(device)
+    # for name, param in model.named_parameters():
+    #     print(f"{name}: {param.shape}")
+    optimizer = opt.Adam(model.parameters(), lr=LR)
+    criterion = nn.CrossEntropyLoss()
+    for epoch in range(NUM_EPOCHS):
+        model.train()
+        total_loss = 0.0
+        correct = 0
+        total = 0
+        # Create a tqdm progress bar for the training batches
+        with tqdm(train_loader, unit="batch") as t_bar:
+            for x, y in t_bar:
+                x, y = x.to(device), y.to(device)
+                outputs = model(x)
+                loss = criterion(outputs, y)
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+                total_loss += loss.item()
+                predicted = torch.argmax(outputs, dim=-1)
+                total += y.size(0)
+                correct += (predicted == y).sum().item()
+
+                # Update tqdm progress bar description
+                t_bar.set_description(f"Epoch {epoch+1}/{NUM_EPOCHS}")
+                t_bar.set_postfix(loss=total_loss / (total + 1e-8), accuracy=100 * correct / total)
+
+        # Print epoch-level information
+        print(f"Epoch {epoch+1}/{NUM_EPOCHS} - Loss: {total_loss / (total + 1e-8):.4f}, Accuracy: {100 * correct / total:.2f}%")
